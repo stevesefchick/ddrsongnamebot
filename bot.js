5e368588a9ef9af6cbf24bb5b9ad3ae6e69a6a40
Steve Sefchick <stevesefchick@gmail.com>
2019-11-06 22:26:01 -0500

    added some code to cut down randomly on tweeting

--- bot.js
+++ bot.js
@@ -35,16 +35,32 @@ var artistName = require('./artistFunctions/artistName.js');
     return songName;
   }
 
+  var randomnumber = function(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   var postThatBadSong = function()
   {
       var post = generatePost();
   
       console.log(post);
-      
-      // comment this out when testing!
-      Twitter.post('statuses/update', {status: post}, function(err, data, response) {
+
+      //need to cut down on posting....
+      //if 1, don't post!
+      if (randomnumber(3) == 1)
+      {
+          console.log("fart noises");
+      }
+      //otherwise, post!
+      else
+      {
+        // comment this out when testing!
+        Twitter.post('statuses/update', {status: post}, function(err, data, response) {
           console.log(data)
-      })
+    })
+      }
+
+
       
   }
 
